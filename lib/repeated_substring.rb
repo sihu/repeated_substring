622b582ecab8f1c892148ff7424982e957d76fec
Simon Huber <sihu89@gmail.com>
2015-07-10 15:27:22 +0200

    Implement Algorithm

--- lib/repeated_substring.rb
+++ lib/repeated_substring.rb
@@ -1,6 +1,25 @@
 class RepeatedSubstring
   def find_repeated_substring(string)
-    # TODO: implement the function
-    1 + 2
+    substring_length = 2
+    longest_substring = "NONE"
+    (2..(string.length/2)).each do |substring_length|
+      new_longest_substring = string_has_substring_of_length(string,substring_length) 
+      if new_longest_substring == "NONE" 
+        break;
+      end
+      longest_substring = new_longest_substring
+   end
+   longest_substring
   end
+
+  def string_has_substring_of_length(string, searchLength)
+     lastsubstring = "NONE"
+     (0..(string.length-1-searchLength)).each do |startchar|
+        substring  = string[startchar..(startchar+searchLength-1)]
+        if string.scan(substring).length > 1
+          lastsubstring =  substring
+        end
+     end
+     lastsubstring
+  end 
 end
